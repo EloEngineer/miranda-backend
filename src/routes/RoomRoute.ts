c0726261d8ad9a361039dfab949b4384004d050c
Diego Prieto <diego.prieto.dev@gmail.com>
2023-05-31 08:32:42 +0200

    RoomRouter done

---
+++ src/routes/RoomRoute.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import {
+  getRooms,
+  getRoomById,
+  createRoom,
+  updateRoom,
+  deleteRoom,
+} from "../controllers/RoomsController";
+
+const RoomRouter = Router();
+
+RoomRouter.get("/", getRooms);
+
+RoomRouter.get("/:id", getRoomById);
+
+RoomRouter.post("/", createRoom);
+
+RoomRouter.put("/:id", updateRoom);
+
+RoomRouter.delete("/:id", deleteRoom);
+
+export default RoomRouter;
