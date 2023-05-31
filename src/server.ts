05fd85b76f303f01e7774dcf2ce012caba78b211
Diego Prieto <diego.prieto.dev@gmail.com>
2023-05-31 08:34:59 +0200

    Implemented the routers to the server ts file

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from "cors";
+
 import UserRouter from "./routes/UserRoute";
+import ContactRouter from "./routes/ContactRoute";
+import BookingRouter from "./routes/BookingRoute";
+import RoomRouter from "./routes/RoomRoute";
 import authRouter from "./controllers/LoginController";
 import passport from "passport";
 import "./services/Login";
@@ -11,10 +15,28 @@ const PORT = 3000;
 app.use(express.json());
 
 app.use("/login", authRouter);
+
 app.use(
-  "/api/users",
+  "/api/v1/users",
   passport.authenticate("jwt", { session: false }),
   UserRouter
+);
+app.use(
+  "/api/v1/rooms",
+  passport.authenticate("jwt", { session: false }),
+  RoomRouter
+);
+
+app.use(
+  "/api/v1/bookings",
+  passport.authenticate("jwt", { session: false }),
+  BookingRouter
+);
+
+app.use(
+  "/api/v1/contact",
+  passport.authenticate("jwt", { session: false }),
+  ContactRouter
 );
 
 export default function startServer() {
