a0923099f3bbdb1709ba93d1dd4cef84038f851c
Diego Prieto <diego.prieto.dev@gmail.com>
2023-05-31 08:33:02 +0200

    ContactRouter done

---
+++ src/routes/ContactRoute.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import {
+  getContacts,
+  getContactById,
+  createContact,
+  updateContact,
+  deleteContact,
+} from "../controllers/ContactController";
+
+const ContactRouter = Router();
+
+ContactRouter.get("/", getContacts);
+
+ContactRouter.get("/:id", getContactById);
+
+ContactRouter.post("/", createContact);
+
+ContactRouter.put("/:id", updateContact);
+
+ContactRouter.delete("/:id", deleteContact);
+
+export default ContactRouter;
