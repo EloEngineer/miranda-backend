4ef5c18bb2abcd4252322af58ecf9cdfdc4e48f3
Diego Prieto <diego.prieto.dev@gmail.com>
2023-05-31 08:23:51 +0200

    Room Controller done

---
+++ src/controllers/RoomsController.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import rooms from "../repositories/rooms";
+import { IRoom } from "../interfaces/RoomInterface";
+
+const getRooms = (_: Request, res: Response) => {
+  try {
+    return res.send(rooms.getAll());
+  } catch (err: any) {
+    return res.sendStatus(500);
+  }
+};
+
+const getRoomById = (req: Request<{ id: string }, IRoom>, res: Response) => {
+  try {
+    return res.send(rooms.getOne(Number(req.params.id)));
+  } catch (err: any) {
+    return res.status(err.status ?? 500).send(err.message);
+  }
+};
+
+const createRoom = (req: Request<{}, IRoom, IRoom>, res: Response) => {
+  try {
+    return res.status(200).send(rooms.create(req.body));
+  } catch (err: any) {
+    return res.status(err.status ?? 500).send(err.message);
+  }
+};
+
+const updateRoom = (
+  req: Request<{ id: string }, IRoom, IRoom>,
+  res: Response
+) => {
+  try {
+    return res.send(rooms.update(Number(req.params.id), req.body));
+  } catch (err: any) {
+    return res.status(err.status ?? 500).send(err.message);
+  }
+};
+
+const deleteRoom = (req: Request<{ id: string }, string>, res: Response) => {
+  try {
+    return res.send(rooms.delete(Number(req.params.id)));
+  } catch (err: any) {
+    return res.status(err.status ?? 500).send(err.message);
+  }
+};
+
+export { getRooms, getRoomById, createRoom, updateRoom, deleteRoom };
